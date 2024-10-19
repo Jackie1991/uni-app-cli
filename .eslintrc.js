0890f4ff22a1d79117378009748ddbf97d680eb0
Jackie <jackiedl@qq.com>
2024-10-19 19:24:20 +0800

    update options config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,9 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier',
-    'plugin:prettier/recommended'
-  ],
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-typescript', '@vue/eslint-config-prettier', 'plugin:prettier/recommended'],
   parserOptions: {
     parser: '@typescript-eslint/parser',
     sourceType: 'module',
