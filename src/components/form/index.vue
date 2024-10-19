0f54f74ccb2e8fc060ff336f8811ef0cac87f754
Jackie Deng <jackiedl@qq.com>
2024-10-19 18:55:35 +0800

    Create index.vue

---
+++ src/components/form/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="vu-form"></div>
+</template>
+
+<script lang="ts" setup>
+defineOptions({
+  name: 'VuForm',
+})
+</script>
+
+<style lang="scss" scoped>
+.vu-form {
+}
+</style>
