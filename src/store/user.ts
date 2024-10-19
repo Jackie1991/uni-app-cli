2a9bee6f8c43e8a2e903614fe27181249116af28
Jackie Deng <jackiedl@qq.com>
2024-10-19 18:50:53 +0800

    Update user.ts

--- src/store/user.ts
+++ src/store/user.ts
@@ -1,1 +1,23 @@
-
+export const useUserStore = defineStore('user', {
+  state: (): UserModuleType => ({
+    admin: false,
+    role: [],
+    permission: [],
+  }),
+  getters: {
+    getAdmin: (state) => state.admin,
+    getRole: (state) => state.role,
+    getPermission: (state) => state.permission,
+  },
+  actions: {
+    setFull(admin: boolean) {
+      this.admin = admin
+    },
+    setRole(role: string[]) {
+      this.role = role
+    },
+    setPermission(permission: string[]) {
+      this.permission = permission
+    },
+  },
+})
